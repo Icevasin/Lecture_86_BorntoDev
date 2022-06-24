16df4b53f076e536faff9abd3bb6b2f7f9bd5f07
Vasin Hiran <vasinhiran2012@gmail.com>
2022-06-24 10:50:53 +0700

    Home Work

---
+++ SortedListApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SortedListApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sortedList = new SortedList();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            sortedList.Add(int.Parse(keytxt.Text),valuetxt.Text);
+
+            ICollection collection = sortedList.Keys;
+            string All = "";
+            foreach(int key in collection)
+            {
+                All = All + (sortedList[key].ToString())+" ";
+            }
+            MessageBox.Show(All);
+        }
+    }
+}
